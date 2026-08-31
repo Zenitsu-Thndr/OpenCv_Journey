a02c48440654a84bc72dda42ab35e36e05518c6a
Zenitsu <velocityxff@gmail.com>
2026-08-31 17:03:57 +0530

    BGR Done

--- 04_Color_Channels/main.py
+++ 04_Color_Channels/main.py
@@ -11,9 +11,19 @@ blue, green, red = cv2.split(image)
 # cv2.imshow("Original", image)
 
 
-cv2.imshow("Blue", blue)
-cv2.imshow("Green", green)
-cv2.imshow("Red", red)
+# cv2.imshow("Blue", blue)
+# cv2.imshow("Green", green)
+# cv2.imshow("Red", red)
+
+
+# blue[:] = cv2.add(green, 50)
+blue[:] = green
+
+# blue[:] = cv2.addWeighted(blue, 0.5, green, 0.5, 0)
+
+modified = cv2.merge([blue, green, red])
+
+cv2.imshow("Modified", modified)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
