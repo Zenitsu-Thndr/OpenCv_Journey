5f1ce8921079011b57f7fb4f1e686b4fb9f548cd
Zenitsu <velocityxff@gmail.com>
2026-09-01 16:31:49 +0530

    Created HSV mask for green only

--- 04_Color_Channels/main.py
+++ 04_Color_Channels/main.py
@@ -28,11 +28,29 @@ blue, green, red = cv2.split(image)
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+'''
 h, s, v = cv2.split(hsv)
 
 cv2.imshow("Hue", h)
 cv2.imshow("Saturation", s)
 cv2.imshow("Value", v)
+'''
+
+
+
+lower_green = np.array([35, 50, 50])
+upper_green = np.array([85, 255, 255])
+
+mask = cv2.inRange(hsv, lower_green, upper_green)
+
+# result = cv2.bitwise_and(image, image, mask=mask)
+
+cv2.imshow("Original", image)
+cv2.imshow("Mask", mask)
+
+
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
