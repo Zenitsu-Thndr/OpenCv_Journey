aa0c39d81b9d2e6b8c132d9761f2a0ae36e98f3f
Zenitsu <velocityxff@gmail.com>
2026-09-05 17:28:29 +0530

    Webcam Done

--- 05_Webcam_Viewer/main.py
+++ 05_Webcam_Viewer/main.py
@@ -1,14 +1,26 @@
 import cv2
-import numpy as np
 
-#importing image
-image = cv2.imread("05_Webcam_Viewer/images/loki.jpg")
+cap = cv2.VideoCapture(0)
 
 
+if not cap.isOpened():
+    print("Could not open camera")
+    exit()
 
 
-cv2.imshow("Original", image)
+while True:
+    ret, frame = cap.read()
 
-cv2.waitKey(0)
+    if not ret:
+        print("Failed To capture frame")
+        break
+
+    cv2.imshow("Webcam", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
+
+cap.release()
 cv2.destroyAllWindows()
-
