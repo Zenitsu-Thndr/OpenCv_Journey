5f9cf820c1999dc907d1550a751c569598ea0d7d
Zenitsu <velocityxff@gmail.com>
2026-09-07 17:23:17 +0530

    Created mask for detecting red

--- 07_Color_Detector/main.py
+++ 07_Color_Detector/main.py
@@ -16,7 +16,19 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow("Color Detector", hsv)
+    lower_red1 = np.array([0, 50, 50])
+    upper_red1 = np.array([10, 255, 255])
+    
+    lower_red2 = np.array([170, 50, 50])
+    upper_red2 = np.array([179, 255, 255])
+    
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    
+    mask = cv2.bitwise_or(mask1, mask2)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("Color Detector", result)
 
     key = cv2.waitKey(1) & 0xFF
 
