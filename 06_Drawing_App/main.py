75e374a5865680cc0f073893eecb377a786f479e
Zenitsu <velocityxff@gmail.com>
2026-09-07 16:15:50 +0530

    Simple Drawing Canvas

--- 06_Drawing_App/main.py
+++ 06_Drawing_App/main.py
@@ -4,7 +4,34 @@ import numpy as np
 canvas = np.zeros((600, 800, 3), dtype=np.uint8)
 
 
+drawing = False
+previous_point = None
 
-cv2.imshow("Drawing App", canvas)
+def mouse_event(event, x, y, flags, param):
+    global drawing, previous_point
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        previous_point = (x, y)
+
+    elif event == cv2.EVENT_MOUSEMOVE and drawing:
+        cv2.line(canvas, previous_point, (x, y), (255, 255, 255), 3)
+        previous_point = (x, y)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        previous_point = None
+
+
+cv2.namedWindow("Drawing App")
+cv2.setMouseCallback("Drawing App", mouse_event)
+
+while True:
+    cv2.imshow("Drawing App", canvas)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
