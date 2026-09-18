7ef53db01c57c35b366d4dc6602b5dbbb1368335
Zenitsu <velocityxff@gmail.com>
2026-09-18 16:55:51 +0530

    Edge Detector Done

--- 11_Edge_Detector/main.py
+++ 11_Edge_Detector/main.py
@@ -13,11 +13,26 @@ while True:
         print("Failed to capture frame")
         break
 
+    smooth = cv2.bilateralFilter(frame, 9, 75, 75)
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    edges = cv2.Canny(gray, 50, 200)
+    edges = cv2.Canny(gray, 100, 200)
 
-    cv2.imshow("Edge Detector", edges)
+    edges = cv2.threshold(
+        edges,
+        100,
+        255,
+        cv2.THRESH_BINARY
+    )[1]
+
+    # edges = cv2.bitwise_not(edges)
+
+    cartoon = cv2.bitwise_and(smooth, smooth, mask=edges)
+
+    # cv2.imshow("Smooth", smooth)
+    # cv2.imshow("Edges", edges)
+    cv2.imshow("Cartoon Camera", cartoon)
 
     key = cv2.waitKey(1) & 0xFF
 
