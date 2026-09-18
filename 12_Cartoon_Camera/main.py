011295516216114dd495f9720f1d83b5e6e23e05
Zenitsu <velocityxff@gmail.com>
2026-09-18 16:36:13 +0530

    Cartoon Camera

---
+++ 12_Cartoon_Camera/main.py
@@ -0,0 +1,28 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Could not open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to capture frame")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    edges = cv2.Canny(gray, 50, 200)
+
+    cv2.imshow("Edge Detector", edges)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
