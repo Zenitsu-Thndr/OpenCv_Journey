b595bcd546568ed244c8f11f03550b0c968beda6
Zenitsu <velocityxff@gmail.com>
2026-09-19 16:44:18 +0530

    BG Blur Project Done

--- 15_BG_Blur/main.py
+++ 15_BG_Blur/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 
 cap = cv2.VideoCapture(0)
 
@@ -13,13 +14,48 @@ while True:
         print("Failed to capture frame")
         break
 
+
+    height, width = frame.shape[:2]
+
     blurred = cv2.GaussianBlur(
         frame,
         (31, 31),
         0
     )
 
-    cv2.imshow("Blurred Background", blurred)
+
+    mask = numpy.zeros((height, width), dtype=numpy.uint8)
+
+    cv2.rectangle(
+        mask,
+        (width // 4, height // 8),
+        (3 * width // 4, 7 * height // 8),
+        255,
+        -1
+    )
+
+    foreground = cv2.bitwise_and(
+        frame,
+        frame,
+        mask=mask
+    )
+
+    background_mask = cv2.bitwise_not(mask)
+
+    background = cv2.bitwise_and(
+        blurred,
+        blurred,
+        mask=background_mask
+    )
+
+    result = cv2.add(
+        foreground,
+        background
+    )
+
+    # cv2.imshow("Blurred Background", blurred)
+    # cv2.imshow("Mask", mask)
+    cv2.imshow("Result", result)
 
     key = cv2.waitKey(1) & 0xFF
 
