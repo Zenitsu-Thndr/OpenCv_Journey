b031e10714782e80a4f3918f73ed29c6096b18ca
Zenitsu <velocityxff@gmail.com>
2026-09-21 17:30:53 +0530

    Hand Landmarks Done

--- 19_Hand_Landmarks/main.py
+++ 19_Hand_Landmarks/main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import math
 
 
 mp_hands = mp.solutions.hands
@@ -34,6 +35,46 @@ while True:
     if results.multi_hand_landmarks:
 
         for hand_landmarks in results.multi_hand_landmarks:
+
+            wrist = hand_landmarks.landmark[0]
+            index_tip = hand_landmarks.landmark[8]
+
+            height, width = frame.shape[:2]
+
+            wrist_px = (
+                int(wrist.x * width),
+                int(wrist.y * height)
+            )
+
+            index_px = (
+                int(index_tip.x * width),
+                int(index_tip.y * height)
+            )
+
+            distance = math.sqrt(
+                (index_px[0] - wrist_px[0]) ** 2 +
+                (index_px[1] - wrist_px[1]) ** 2
+            )
+
+
+            cv2.line(
+                frame,
+                wrist_px,
+                index_px,
+                (255, 0, 0),
+                2
+            )
+            
+            cv2.putText(
+                frame,
+                f"{distance:.1f} px",
+                (20, 40),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.8,
+                (255, 0, 0),
+                2
+            )
+
 
             mp_draw.draw_landmarks(
                 frame,
